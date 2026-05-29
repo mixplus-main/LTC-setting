1c4d220a91c4cae38229f8ecdca86417f939b874
mixplus <wuyouqitian@gmail.com>
2026-05-29 18:53:57 +0900

    GUIの変更

--- src/main/java/com/mixplus/GUI/GUI.java
+++ src/main/java/com/mixplus/GUI/GUI.java
@@ -99,9 +99,32 @@ public class GUI extends JFrame{
         test.setBounds(500,500,30,30);
         test.setFont(new Font("Arial", Font.PLAIN, 18));
 
+
+        //diff calc gui
+        Label Diff = new Label("   Diff");
+        Diff.setBounds(650,210,70,20);
+        Diff.setFont(new Font("Arial", Font.PLAIN, 20));
+
+        Label Diff_z = new Label("Z:");
+        Diff_z.setBounds(580,235,20,15);
+        Diff_z.setFont(new Font("Arial", Font.PLAIN, 18));
+
+        TextBox Diff_z_box = new TextBox();
+        Diff_z_box.setBounds(600,230,200,26);
+        Diff_z_box.setFont(new Font("Arial", Font.PLAIN, 15));
+
+        Label Diff_y = new Label("Y:");
+        Diff_y.setBounds(580,265,20,15);
+        Diff_y.setFont(new Font("Arial", Font.PLAIN, 18));
+
+        TextBox Diff_y_box = new TextBox();
+        Diff_y_box.setBounds(600,260,200,26);
+        Diff_y_box.setFont(new Font("Arial", Font.PLAIN, 15));
+
         test.addActionListener(e -> {
             System.out.println(Origin_y_box.getText());
             System.out.println();
+
         });
 
 
@@ -126,6 +149,11 @@ public class GUI extends JFrame{
         frame.add(Target_z_box);
         frame.add(Target_y);
         frame.add(Target_y_box);
+        frame.add(Diff);
+        frame.add(Diff_z);
+        frame.add(Diff_z_box);
+        frame.add(Diff_y);
+        frame.add(Diff_y_box);
         frame.add(test);
     }
 
